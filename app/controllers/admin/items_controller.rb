2a814758f7dcd8b3a201f3462102f1d1285ee12e
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-10-17 07:52:01 +0000

    [add]controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,50 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @genre = Genre.all
+    @items = Item.page(params[:page])
+  end
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @genres = Genre.all
+    if @item.save
+      flash[:notice] = "商品情報を登録しました"
+      redirect_to admin_item_path(@item)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    if @item.update(item_params)
+      flash[:notice] = "商品情報を変更しました"
+      redirect_to admin_item_path(@item)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_image, :name, :introduction, :genre_id, :without_tax, :is_active)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,26 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def show
+    @order = Order.find(params[:id])
+    @total = @order.order_details.inject(0) {|sum, order_detail| sum + order_detail.total_price }
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    
+    if @order.update(order_params)
+      if @order.status == "check"
+        @order_details.update_all(making_status: "wait")
+      end
+    redirect_to admin_order_path(@order)
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
