941661ab39ac3ad1d3307b41dad17929531d6744
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-10-19 04:32:00 +0000

    [fix]ja.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_17_051336) do
+ActiveRecord::Schema.define(version: 2023_10_18_051208) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,10 +41,6 @@ ActiveRecord::Schema.define(version: 2023_10_17_051336) do
   end
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.string "postcode", null: false
-    t.string "address", null: false
-    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -62,9 +58,6 @@ ActiveRecord::Schema.define(version: 2023_10_17_051336) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "customer_id", null: false
-    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -75,14 +68,6 @@ ActiveRecord::Schema.define(version: 2023_10_17_051336) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "postcode", null: false
-    t.string "address", null: false
-    t.string "phone_number", null: false
-    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -90,40 +75,27 @@ ActiveRecord::Schema.define(version: 2023_10_17_051336) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
   end
 
   create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id", null: false
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "without_tax", null: false
-    t.integer "genres_id", null: false
     t.boolean "is_active", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "order_details", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "order_id", null: false
-    t.integer "amount", null: false
-    t.integer "include_tax", null: false
-    t.integer "making_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.integer "total_price", null: false
-    t.string "name", null: false
-    t.string "postcode", null: false
-    t.string "addresses", null: false
-    t.integer "payment_method", default: 0, null: false
-    t.integer "postage", null: false
-    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
