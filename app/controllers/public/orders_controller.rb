2f0924f77ffb1b852177608631b673341e2dce9e
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-10-19 06:41:38 +0000

    orders-add

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,9 +3,11 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @customer = current_customer
+    
   end
   
   def confirm
+    
   end
   
   def complete
