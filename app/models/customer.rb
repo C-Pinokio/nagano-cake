e0d76a501a5c91be82925cb189ef2ab72d9a877d
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-10-20 07:51:30 +0000

    [add]admin/customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,14 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   
+   def full_name
+    "#{last_name} #{first_name}"
+   end
+   
+   def full_name_kana
+     "#{last_name_kana} #{first_name_kana}"
+   end
+  
   def active_for_authentication?
     # is_deletedがfalseならtrueを返すようにしている
     super && (is_deleted == false)
