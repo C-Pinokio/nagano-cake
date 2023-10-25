0e4438908eac2f8554d707feae82573ec8bb6354
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-10-25 06:06:27 +0000

    顧客検索&顧客注文履歴一覧準備

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,10 +4,12 @@ class Public::SearchesController < ApplicationController
     genre_id = params[:genre_id]
     @item_searches = Item.where(genre_id: genre_id)
     @item_searches_all =  @item_searches.page(params[:page])
-  end
-  
-  def item_search
     @word = params[:word]
     @items = Item.looks(params[:word])#検索ワードparams[:word]を参照してデータを検索し、インスタンス変数@ItemsにItemモデル内での検索結果を代入。
+    @genres = Genre.all
   end
+  
+  def customer_search
+  end
+  
 end
