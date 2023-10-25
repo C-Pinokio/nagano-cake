a27c52b0a90453870220316950253cb6a5f0e6a8
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-10-25 00:45:13 +0000

    ジャンル機能実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,5 +1,8 @@
 class Public::SearchesController < ApplicationController
     
   def search
+    @genre_id = params[:genre_id]
+    @items = Item.where(genre_id: @genre_id)
+    @items_all = @items.page(params[:page])
   end
 end
